cbc68eb7c0cc3650211812ce1f1399bcf555b70e
Jakub Taglewski <255713@edu.p.lodz.pl>
2026-06-15 16:43:29 +0200

    Dodano funkcję detekcji ustawienia pleców

--- src/pose_module.py
+++ src/pose_module.py
@@ -2,15 +2,17 @@ import cv2
 import mediapipe as mp
 import math
 
+
 class PoseDetector:
-    def __init__(self, mode=False, up_body=False, smooth=True, detection_con=0.5, track_con=0.5):
+    def __init__(self, mode=False, up_body=False, smooth=True,
+                 detection_con=0.5, track_con=0.5, complexity=1):
         self.mp_draw = mp.solutions.drawing_utils
         self.mp_pose = mp.solutions.pose
-        self.pose = self.mp_pose.Pose(static_image_mode=mode, 
-                                     model_complexity=1, 
-                                     smooth_landmarks=smooth, 
-                                     min_detection_confidence=detection_con, 
-                                     min_tracking_confidence=track_con)
+        self.pose = self.mp_pose.Pose(static_image_mode=mode,
+                                      model_complexity=complexity,
+                                      smooth_landmarks=smooth,
+                                      min_detection_confidence=detection_con,
+                                      min_tracking_confidence=track_con)
         self.lm_list = []
 
     def find_pose(self, img, draw=True):
@@ -35,7 +37,7 @@ class PoseDetector:
     def find_angle(self, img, p1, p2, p3, draw=True):
         if len(self.lm_list) == 0:
             return 0
-        
+
         # Pobranie koordynatów punktów (landmarks)
         x1, y1 = self.lm_list[p1][1:]
         x2, y2 = self.lm_list[p2][1:]
@@ -43,13 +45,13 @@ class PoseDetector:
 
         # Obliczenie kąta
         angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
-        
+
         if angle < 0:
             angle += 360
-            
+
         if angle > 180:
             angle = 360 - angle
-            
+
         # Rysowanie na obrazie
         if draw:
             cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
@@ -60,6 +62,6 @@ class PoseDetector:
             cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
             cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
             cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
-            cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50), 
+            cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50),
                         cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
         return angle
